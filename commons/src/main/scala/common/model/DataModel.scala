9d9179e683ad29ccd966eb3f05abd0e78cfe892f
UriSasuke <919812366@qq.com>
2019-08-03 21:47:40 +0800

    hive自动生成数据，spark操作hive

---
+++ commons/src/main/scala/common/model/DataModel.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2018. Atguigu Inc. All Rights Reserved.
+ */
+
+package common.model
+
+//***************** 输入表 *********************
+
+/**
+  * 用户访问动作表
+  *
+  * @param date               用户点击行为的日期
+  * @param user_id            用户的ID
+  * @param session_id         Session的ID
+  * @param page_id            某个页面的ID
+  * @param action_time        点击行为的时间点
+  * @param search_keyword     用户搜索的关键词
+  * @param click_category_id  某一个商品品类的ID
+  * @param click_product_id   某一个商品的ID
+  * @param order_category_ids 一次订单中所有品类的ID集合
+  * @param order_product_ids  一次订单中所有商品的ID集合
+  * @param pay_category_ids   一次支付中所有品类的ID集合
+  * @param pay_product_ids    一次支付中所有商品的ID集合
+  * @param city_id            城市ID
+  */
+case class UserVisitAction(date: String,
+                           user_id: Long,
+                           session_id: String,
+                           page_id: Long,
+                           action_time: String,
+                           search_keyword: String,
+                           click_category_id: Long,
+                           click_product_id: Long,
+                           order_category_ids: String,
+                           order_product_ids: String,
+                           pay_category_ids: String,
+                           pay_product_ids: String,
+                           city_id: Long
+                          )
+
+/**
+  * 用户信息表
+  *
+  * @param user_id      用户的ID
+  * @param username     用户的名称
+  * @param name         用户的名字
+  * @param age          用户的年龄
+  * @param professional 用户的职业
+  * @param city         用户所在的城市
+  * @param sex          用户的性别
+  */
+case class UserInfo(user_id: Long,
+                    username: String,
+                    name: String,
+                    age: Int,
+                    professional: String,
+                    city: String,
+                    sex: String
+                   )
+
+/**
+  * 产品表
+  *
+  * @param product_id   商品的ID
+  * @param product_name 商品的名称
+  * @param extend_info  商品额外的信息
+  */
+case class ProductInfo(product_id: Long,
+                       product_name: String,
+                       extend_info: String
+                      )
+
+/**
+  * 聚合统计表
+  *
+  * @param taskid                       当前计算批次的ID
+  * @param session_count                所有Session的总和
+  * @param visit_length_1s_3s_ratio     1-3sSession访问时长占比
+  * @param visit_length_4s_6s_ratio     4-6sSession访问时长占比
+  * @param visit_length_7s_9s_ratio     7-9sSession访问时长占比
+  * @param visit_length_10s_30s_ratio   10-30sSession访问时长占比
+  * @param visit_length_30s_60s_ratio   30-60sSession访问时长占比
+  * @param visit_length_1m_3m_ratio     1-3mSession访问时长占比
+  * @param visit_length_3m_10m_ratio    3-10mSession访问时长占比
+  * @param visit_length_10m_30m_ratio   10-30mSession访问时长占比
+  * @param visit_length_30m_ratio       30mSession访问时长占比
+  * @param step_length_1_3_ratio        1-3步长占比
+  * @param step_length_4_6_ratio        4-6步长占比
+  * @param step_length_7_9_ratio        7-9步长占比
+  * @param step_length_10_30_ratio      10-30步长占比
+  * @param step_length_30_60_ratio      30-60步长占比
+  * @param step_length_60_ratio         大于60步长占比
+  */
+case class SessionAggrStat(taskid: String,
+                           session_count: Long,
+                           visit_length_1s_3s_ratio: Double,
+                           visit_length_4s_6s_ratio: Double,
+                           visit_length_7s_9s_ratio: Double,
+                           visit_length_10s_30s_ratio: Double,
+                           visit_length_30s_60s_ratio: Double,
+                           visit_length_1m_3m_ratio: Double,
+                           visit_length_3m_10m_ratio: Double,
+                           visit_length_10m_30m_ratio: Double,
+                           visit_length_30m_ratio: Double,
+                           step_length_1_3_ratio: Double,
+                           step_length_4_6_ratio: Double,
+                           step_length_7_9_ratio: Double,
+                           step_length_10_30_ratio: Double,
+                           step_length_30_60_ratio: Double,
+                           step_length_60_ratio: Double
+                          )
---
+++ mock/src/main/scala/mock/MockDataGenerate.scala
@@ -0,0 +1,161 @@
+package mock
+
+import java.util.UUID
+
+import common.constant.Constants
+import common.model.{ProductInfo, UserInfo, UserVisitAction}
+import common.utils.{DateUtils, StringUtils}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+/**
+  * Created by Enzo Cotter on 2019/7/27.
+  */
+object MockDataGenerate {
+
+
+  /**
+    * UserInfo数据自动生成
+    *
+    * @return
+    */
+  private def mockUserInfo(): Array[UserInfo] = {
+
+    val rows = new ArrayBuffer[UserInfo]()
+    val sexes = Array("male", "female")
+    val random = new Random()
+
+    for (i <- 0 to 10000) {
+      val user_id = i
+      val username = "user_" + i
+      val name = "name_" + i
+      val age = random.nextInt(60)
+      val professional = "professional_" + random.nextInt(10000)
+      val city = "city_" + random.nextInt(1000)
+      val sex = sexes(random.nextInt(2))
+      rows += UserInfo(user_id, username, name, age, professional, city, sex)
+    }
+    rows.toArray
+  }
+
+  /**
+    * ProductInfo数据自动生成
+    *
+    * @return
+    */
+  private def mockProductInfo(): Array[ProductInfo] = {
+    val rows = new ArrayBuffer[ProductInfo]()
+    val random = new Random()
+    val productStatus = Array(0, 1)
+    for (i <- 0 to 10000) {
+
+      val product_id = i
+      val product_name = "product_" + i
+      val extend_info = "{\"product_status\": " + productStatus(random.nextInt(2)) + "}"
+      rows += ProductInfo(product_id, product_name, extend_info)
+    }
+    rows.toArray
+  }
+
+  /**
+    * UserVisitAction数据自动生成
+    *
+    * @return
+    */
+  private def mockUserVisitAction(): Array[UserVisitAction] = {
+    val searchKeywords = Array("华为手机", "联想笔记本", "小龙虾", "卫生纸", "吸尘器", "Lamer", "机器学习", "苹果", "洗面奶", "保温杯")
+
+    //yyyy-MM-dd
+    val date = DateUtils.getTodayDate()
+    val actions = Array("search", "click", "order", "pay")
+    val random = new Random()
+    val rows = ArrayBuffer[UserVisitAction]()
+
+    //一共10000个用户（session有重复）
+    for (i <- 0 to 10000) {
+
+      val userid = random.nextInt(10000)
+      // 每个用户产生100个session
+      for (j <- 0 to 100) {
+        // 不可变的，全局的，独一无二的128bit长度的标识符，用于标识一个session，体现一次会话产生的sessionId是独一无二的
+        val sessionid = UUID.randomUUID().toString().replace("-", "")
+
+        // 在yyyy-MM-dd后面添加一个随机的小时时间（0-23）
+        val baseActionTime = date + " " + random.nextInt(23)
+        // 每个(userid + sessionid)生成0-100000条用户访问数据
+        for (k <- 0 to random.nextInt(100000)) {
+          val pageid = random.nextInt(100)
+          // 在yyyy-MM-dd HH后面添加一个随机的分钟时间和秒时间
+          val actionTime = baseActionTime + ":" + StringUtils.fulfuill(String.valueOf(random.nextInt(59))) + ":" + StringUtils.fulfuill(String.valueOf(random.nextInt(59)))
+          var searchKeyword: String = null
+          var clickCategoryId: Long = -1L
+          var clickProductId: Long = -1L
+          var orderCategoryIds: String = null
+          var orderProductIds: String = null
+          var payCategoryIds: String = null
+          var payProductIds: String = null
+          val cityid = random.nextInt(10).toLong
+
+          // 随机确定用户在当前session中的行为
+          val action = actions(random.nextInt(4))
+
+          action match {
+            case "search" => searchKeyword = searchKeywords(random.nextInt(10))
+            case "click" => clickCategoryId = random.nextInt(100).toLong
+              clickProductId = String.valueOf(random.nextInt(100)).toLong
+            case "order" => orderCategoryIds = random.nextInt(100).toString
+              orderProductIds = random.nextInt(100).toString
+            case "pay" => payCategoryIds = random.nextInt(100).toString
+              payProductIds = random.nextInt(100).toString
+          }
+
+          rows += UserVisitAction(date, userid, sessionid,
+            pageid, actionTime, searchKeyword,
+            clickCategoryId, clickProductId,
+            orderCategoryIds, orderProductIds,
+            payCategoryIds, payProductIds, cityid)
+
+        }
+      }
+
+    }
+    rows.toArray
+  }
+
+  def main(args: Array[String]): Unit = {
+    //spark配置
+    val sparkConf = new SparkConf().setAppName("Mock Data").setMaster("local[*]")
+    //spark sql客户端
+    val spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
+
+    val userInfo = this.mockUserInfo()
+    val productInfo = this.mockProductInfo()
+    val userVisitAction = this.mockUserVisitAction()
+
+    val userInfoRDD = spark.sparkContext.makeRDD(userInfo)
+    val productInfoRDD = spark.sparkContext.makeRDD(productInfo)
+    val userVisitActionRDD = spark.sparkContext.makeRDD(userVisitAction)
+
+    import spark.implicits._
+    insertHive(spark, Constants.USER_INFO_TABLE, userInfoRDD.toDF)
+    insertHive(spark, Constants.PRODUCT_INFO_TABLE, productInfoRDD.toDF())
+    insertHive(spark, Constants.USER_VISIT_ACTION_TABLE, userVisitActionRDD.toDF())
+    spark.close
+  }
+
+  /**
+    * 插入数据到hive
+    * @param sparkSession
+    * @param tableName
+    * @param dataRDD
+    */
+  def insertHive(sparkSession: SparkSession, tableName: String, dataDF: DataFrame) ={
+    val sql = "DROP TABLE IF EXISTS " + tableName
+    sparkSession.sql(sql)
+    dataDF.write.saveAsTable(tableName)
+  }
+
+}
